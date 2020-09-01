e29ef5ea5d71c3874b4ff2fc4052b28d5ab9c83a
Mikaël Barbero <mikael.barbero@gmail.com>
2020-09-01 10:33:36 +0200

    Added access-log configuration to startup script

--- start.sh
+++ start.sh
@@ -3,8 +3,12 @@
 export NOTARIZATION_APPLEID_USERNAME="appleid"
 export NOTARIZATION_APPLEID_PASSWORD="appleid_password"
 
-JAR_FILE=$(find . -name "target/macos-notarization-service*-runner.jar" | sort | tail -n 1)
+JAR_FILE=$(find . -name "macos-notarization-service*.jar" | sort | tail -n 1)
 
-"$(/usr/libexec/java_home -v 1.8)/bin/java" -Dquarkus.http.port=8080 \
+"$(/usr/libexec/java_home -v 1.8)/bin/java" -Dquarkus.http.port=8383 \
 	-Dquarkus.log.category.\"org.eclipse.cbi\".level=INFO \
+	-Dquarkus.http.access-log.enabled=true \
+	-Dquarkus.http.access-log.pattern=combined \
+	-Dquarkus.http.access-log.log-to-file=true \
+	-Dquarkus.http.access-log.log-directory="$(cd "$(dirname "${0}")" && pwd)/log" \
 	-jar "${JAR_FILE}"
